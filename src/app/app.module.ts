b541e00b770ab97b3fe00ee2435a31ebfa427ea5
Isuru Tennakoon <isurutennakoon95@gmail.com>
2019-01-28 23:32:43 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import {Router, RouterModule, Routes} from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+
+const appRoutes :Routes = [
+    {
+      path: 'notes',
+      component:NotesComponent
+    },
+    {
+      path: 'feedback',
+      component: FeedbackComponent
+    },
+    {
+      path: '',
+      component: NotesComponent,
+      pathMatch: 'full'
+    },
+    {
+      path: '**',
+      component: NotfoundComponent
+    }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes,{enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
